c4c60991e5952155e439729adcb2699242460fe9
alper demirci <alper.demirci@gmail.com>
2017-08-24 02:13:04 -0400

    Guard the array count to at least 1. Naming convention

--- findThreeMaxNumber.playground/Contents.swift
+++ findThreeMaxNumber.playground/Contents.swift
@@ -2,24 +2,27 @@
 
 import UIKit
 
+//question: Find the three greatest number in the array and return the multiplication of them
+
 func greatestThree(numbers: [Int]) -> Int {
-    var first = numbers[0]
-    var second = 0
-    var third = 0
+    guard numbers.count >= 3 else {
+        return 0
+    }
+    var firstGreatest = numbers[0]
+    var secondGreatest = 0
+    var thirdGreatest = 0
     for i in 0...numbers.count-1 {
-        if numbers[i] > first {
-            third = second
-            second = first
-            first = numbers[i]
-        }
-        if (numbers[i] > second) && (numbers[i] < first) {
-            third = second
-            second = numbers[i]
-        }
-        if (numbers[i] > third) && (numbers[i] < second) {
-            third == numbers[i]
+        if numbers[i] > firstGreatest {
+            thirdGreatest = secondGreatest
+            secondGreatest = firstGreatest
+            firstGreatest = numbers[i]
+        } else if (numbers[i] > secondGreatest) && (numbers[i] < firstGreatest) {
+            thirdGreatest = secondGreatest
+            secondGreatest = numbers[i]
+        } else if (numbers[i] > thirdGreatest) && (numbers[i] < secondGreatest) {
+            thirdGreatest == numbers[i]
         }
     }
-    return first * second * third
+    return firstGreatest * secondGreatest * thirdGreatest
 }
-greatestThree(numbers: [1, 3, 2, 5, 7, 4, 9, 11])
+greatestThree(numbers: [1, 0, 4, 6, 7, 8])
