131b8a6babab3c4bc9f5c17ff4a0a2591c420b65
alper demirci <alper.demirci@gmail.com>
2017-08-24 01:43:17 -0400

    Improved the algorithm to handle the array and decreased the nested loops

--- findThreeMaxNumber.playground/Contents.swift
+++ findThreeMaxNumber.playground/Contents.swift
@@ -3,28 +3,26 @@
 import UIKit
 
 func greatestThree(numbers: [Int]) -> Int {
-    var first = 0
+    var first = numbers[0]
     var second = 0
     var third = 0
     var num = 0
     for i in 0...numbers.count-1 {
         num = numbers[i]
-        if num > third {
-            if num > second {
-                if third > second {
-                    if num > first {
-                        second = first
-                        first = num
-                    } else {
-                        second = num
-                    }
-                }
-                else {
-                    third = num
-                }
-            }
+        if numbers[i] > first {
+            third = second
+            second = first
+            first = numbers[i]
+        }
+        if (numbers[i] > second) && (numbers[i] < first) {
+            third = second
+            second = numbers[i]
+        }
+        if (numbers[i] > third) && (numbers[i] < second) {
+            third == numbers[i]
         }
     }
+    
     return first * second * third
 }
-greatestThree(numbers: [1, 3, 2, 7, 4, 9, 11])
+greatestThree(numbers: [1, 3, 2, 5, 7, 4, 9, 11])
